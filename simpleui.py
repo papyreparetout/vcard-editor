78a56391d5e7323e901b303655e6d7bca09f8906
papyreparetout <34749810+papyreparetout@users.noreply.github.com>
2021-10-12 18:18:53 +0200

    Add files via upload

--- simpleui.py
+++ simpleui.py
@@ -162,7 +162,7 @@ headings = ['numéro','property','nb param', 'paramètres', 'valeur']
 
 sg.theme('Dark Blue 3')  # please make your windows colorful
 
-# First the window layout
+# layout de la fenêtre principale
 menu_def = [['Fichier',['Ouvrir', 'Save', 'Exit'  ]], ['Editer', ['Modifier','Inserer','Supprimer']]]
 layout = [
           [sg.Menu(menu_def, background_color='lightsteelblue',text_color='navy',
@@ -175,11 +175,9 @@ layout = [
             )],
           ]
 
-
 window = sg.Window("Vcard editor", layout, resizable=True, finalize=True )
-window2_active = False 
-
-#                   , size =(600,600))
+window2_active = False
+window3_active = False 
 
 # event loop
 
@@ -200,13 +198,53 @@ while True:  # Event Loop
             window['Table'].table_frame.pack(expand=True, fill='both')
         except:
             print("pas de fichier choisi")
+            sg.popup('pas de fichier choisi!')
             
 # edition: modification d'une ligne de données
     try:
-        if event == 'Modifier':
+        if not window3_active and event == 'Modifier' and not values['Table'] == []:
             ligne = values['Table']
             print("modification ligne : " + str(ligne[0]))
-    #        data.insert(ligne[0],["insertion"])
+            window3_active = True
+            valinit1 = str(data[ligne[0]][0])
+            valinit2 = str(data[ligne[0]][1])
+            valinit3 = str(data[ligne[0]][2])
+            valinit4 = str(data[ligne[0]][3])
+            valinit5 = str(data[ligne[0]][4])
+            layout3 = [
+            [sg.Text('Données')],
+            [sg.Text('Numéro',size = (15,1)),sg.Input( default_text = valinit1,#)],
+                enable_events=False, key='INNUM')],
+            [sg.Text('property', size =(15, 1)), sg.Input(default_text = valinit2,
+                enable_events=False, key='INPROP')],
+            [sg.Text('nb param', size =(15, 1)), sg.Input(default_text = valinit3,
+                enable_events=False, key='INNBPAR')],
+            [sg.Text('paramètre', size =(15, 1)), sg.Input(default_text = valinit4,
+                enable_events=False, key='INPAR')],
+            [sg.Text('valeur', size =(15, 1)), sg.Input(default_text = valinit5,
+                enable_events=False, key='INVAL')],
+            [sg.Submit(button_text='Modifier', key = 'modif'), sg.Cancel(button_text = 'Annuler', key = 'annul')]
+              ]
+            window3 = sg.Window("Données", layout3)
+            if window3_active:
+                ev3, vals3 = window3.read()
+                print("event3: " + str(ev3) + " values3: "+ str(vals3))
+                if ev3 == sg.WIN_CLOSED or ev3 == 'Exit' or ev3 == 'annul':
+                    window3_active  = False
+                    window3.close()
+                if ev3 == 'modif':
+                    newnum = int(vals3['INNUM'])
+                    newprop = vals3['INPROP']
+                    newnbpar = int(vals3['INNBPAR'])
+                    newparam = []
+                    if vals3['INPAR'] != '': newparam = [vals3['INPAR']]
+                    newvalue = vals3['INVAL']
+                    newdata = [newnum, newprop,newnbpar,newparam,newvalue]
+    # on vient insérer la nouvelle ligne et detruire l'ancienne '                    
+                    data.insert(ligne[0],newdata)
+                    data.pop(ligne[0]+1)
+                    window3_active  = False
+                    window3.close()
             window['Table'].update(data)
                 
     # edition: insertion d'une ligne de données
@@ -258,5 +296,6 @@ while True:  # Event Loop
     # fin du programme
     except:
         print("erreur édition")
+        sg.popup('erreur edition')
         
 window.close()
